4608e9e769c13c3ac6b464221bfcfb02164eb107
Martin P <148612731+martingit22@users.noreply.github.com>
2024-07-19 13:19:42 +0300

    Adding init skeleton refactor

---
+++ src/main/java/com/myp/POM/HomePage.java
@@ -0,0 +1,63 @@
+package com.myp.POM;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+public class HomePage extends ISkillo {
+    public static final String HOME_PAGE_SUFIX = "posts/all";
+    @FindBy(id = "nav-link-login")
+    public WebElement navigationLoginButton; // = driver.findElement(By.id("nav-link-login"));
+    @FindBy(id = "nav-link-new-post")
+    private WebElement navigationNewPostButton;
+    @FindBy(xpath = "//i[@class='fas fa-sign-out-alt fa-lg']")
+    private WebElement navigationLogOutButton;
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    };
+
+    public void openHomePage() {
+        navigateTo(HOME_PAGE_SUFIX);
+    };
+
+    public void clickOnNavigationLoginButton() {
+        waitAndClick(navigationLoginButton);
+    };
+
+    public void clickOnNewPostButton() {
+        waitAndClick(navigationNewPostButton);
+    };
+
+    public void clickOnLogOutButton() {
+        waitAndClick(navigationLogOutButton);
+    };
+
+    public boolean isNewPostButtonToShown() {
+        boolean isButtonShown = false;
+        System.out.println(" ACTION @ The user is verifying if the navigation New Post button is presented");
+        try {
+            wait.until(ExpectedConditions.visibilityOf(navigationNewPostButton));
+            System.out.println("CONFIRM # Navigation New Post button is presented to the user");
+            isButtonShown = true;
+        } catch (TimeoutException e) {
+            System.out.println("ERROR ! The navigation New Post button was not presented to the user");
+        }
+        return isButtonShown;
+    };
+
+    public boolean isLogOutButtonShown() {
+        boolean isButtonShown = false;
+        System.out.println("ACTION @ The user is verifying if the navigation log out button is presented");
+        try {
+            wait.until(ExpectedConditions.visibilityOf(navigationLogOutButton));
+            System.out.println("CONFIRM # Navigation logout button is presented to the user");
+            isButtonShown = true;
+        } catch (TimeoutException e) {
+            System.out.println("ERROR ! The navigation logout button was not presented to the user");
+        }
+        return isButtonShown;
+    };
+}
---
+++ src/main/java/com/myp/POM/ISkillo.java
@@ -0,0 +1,86 @@
+package com.myp.POM;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ISkillo {
+    private final String BASE_URL = "http://training.skillo-bg.com:4200/";
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public ISkillo(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    };
+
+    public void navigateTo(String pageURLsufix) {
+        String currentURL = BASE_URL + pageURLsufix;
+        driver.get(currentURL);
+
+        waitPageTobeFullLoaded();
+    };
+
+    public void waitAndClick(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+
+        element.click();
+        System.out.println("The user has clicked on element");
+
+        waitPageTobeFullLoaded();
+    };
+
+    public void typeTextInField(WebElement element, String inputText) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        element.clear();
+        element.sendKeys(inputText);
+
+        waitPageTobeFullLoaded();
+    };
+
+    //This waiting strategy is very appropriate for a small demo test sites
+    //However if we want to execute quickly scripts a waiting utils will be needed
+    public void waitPageTobeFullLoaded() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("return document.readyState").equals("complete");
+        System.out.println("DOM tree is fully loaded");
+    };
+
+    public boolean isUrlLoaded(String pageURL) {
+        waitPageTobeFullLoaded();
+        return wait.until(ExpectedConditions.urlContains(pageURL));
+    };
+
+    public String getPlaceholder(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        return element.getAttribute("placeholder");
+    };
+
+    public boolean isPlaceholderCorrect(WebElement element, String expectedPlaceholder) {
+        try {
+            String actualPlaceholder = getPlaceholder(element);
+            return expectedPlaceholder.equals(actualPlaceholder);
+        } catch (NoSuchElementException e) {
+            System.out.println("ERROR! The placeholder for the element is not correct or element is not found.");
+            return false;
+        }
+    };
+
+    public boolean isTitleShown(WebElement element) {
+        boolean isTitleShown = false;
+        System.out.println("ACTION @ The user is verifying if the Registration title is shown");
+        try {
+            wait.until(ExpectedConditions.visibilityOf(element));
+            System.out.println("CONFIRM # The Registration title is shown to the user");
+            isTitleShown = true;
+        } catch (NoSuchElementException e) {
+            System.out.println("ERROR ! The title is not presented the user is not on Registration page");
+        }
+        return isTitleShown;
+    };
+}
